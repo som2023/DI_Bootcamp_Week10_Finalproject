669f69831ae36dc4f9d833f7fd6fb2a755e2d98d
som2023 <josephsom44@gmail.com>
2023-03-06 19:35:56 +0000

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { InscriptionComponent } from './components/inscription/inscription.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+   path: 'landing', component : AppComponent
+},
+{
+  path:'login', component : LoginComponent
+ },
+{
+  path:'', component : HomeComponent
+},
+{
+  path:'home', component : HomeComponent
+},
+{
+ path:'login', component : LoginComponent
+},
+{
+  path: 'inscription', component : InscriptionComponent
+}
+// ,{
+//   path : '',
+//   redirectTo : 'accueil',
+//   pathMatch : 'full'
+// },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActualiteComponent } from './components/actualite/actualite.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
+import { CarousselComponent } from './components/caroussel/caroussel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { InscriptionComponent } from './components/inscription/inscription.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ActualiteComponent,
+    PresentationComponent,
+    CarousselComponent,
+    FooterComponent,
+    LoginComponent,
+    InscriptionComponent,
+    HomeComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
